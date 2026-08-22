ec9cb7f4f8accd23a64ba99e95967c317fd56ed3
Sai Krishna Chowdary <kittusontineni@gmail.com>
2026-08-22 16:38:55 +0530

    style: use React Bits DotField settings

--- components/matrix/matrix-background.tsx
+++ components/matrix/matrix-background.tsx
@@ -6,11 +6,10 @@ export function MatrixBackground() {
   return (
     <div aria-hidden className="pointer-events-none fixed inset-0 z-0 overflow-hidden">
       <DotField
-        dotRadius={1.35}
-        dotSpacing={16}
-        cursorRadius={132}
-        bulgeStrength={28}
-        glowRadius={36}
+        dotRadius={1.5}
+        dotSpacing={14}
+        bulgeStrength={67}
+        glowRadius={160}
         sparkle={false}
         waveAmplitude={0}
         gradientFrom="#08733a"
